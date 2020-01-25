be24e8835d5f90a5732ef6fb9f184c92ca4bb3e2
julian <julian@localhost.localdomain>
2020-01-25 13:30:35 -0300

    .FIX steps for p902

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -719,7 +719,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.29, 100.42, 802.4, 94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 99.51, 99.51, 850, 440 }
 
 /**
  * Default Max Feed Rate (mm/s)
